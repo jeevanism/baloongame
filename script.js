2c6d9a25dc5c632b9b438ed079b16c7e65ab53a9
jeevanism <jeevanism@gmail.com>
2025-04-26 19:16:48 +0100

    comment

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
+//  no more in use .. only for canvas learning 
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const balloonImage = new Image();
-balloonImage.src = "balloon.png"; // Replace with your balloon image URL
+balloonImage.src = "balloon.png";
 
 let balloonX = canvas.width / 2 - 25;
 let balloonY = canvas.height - 80;
 let balloonScale = 1;
 let explosionFrame = 0;
+let isExploding = false;
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (explosionFrame < 20) {
-    // Expand balloon as part of explosion
-    balloonScale += 0.1;
-    explosionFrame++;
+  if (isExploding) {
+    if (explosionFrame < 20) {
+      balloonScale += 0.1;
+      explosionFrame++;
+      ctx.drawImage(
+        balloonImage,
+        balloonX,
+        balloonY,
+        50 * balloonScale,
+        80 * balloonScale
+      );
+      requestAnimationFrame(animate);
+    } else {
+      // After explosion complete, clear canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  } else {
+    // Normal static balloon
+    ctx.drawImage(balloonImage, balloonX, balloonY, 50, 80);
+    requestAnimationFrame(animate);
   }
-
-  ctx.drawImage(
-    balloonImage,
-    balloonX,
-    balloonY,
-    50 * balloonScale,
-    80 * balloonScale
-  );
-
-  requestAnimationFrame(animate);
 }
 
+// Start the normal animation after balloon loads
 balloonImage.onload = () => {
   animate();
 };
+
+// Add click listener to pop the balloon
+canvas.addEventListener("click", () => {
+  if (!isExploding) {
+    isExploding = true;
+    explosionFrame = 0;
+    balloonScale = 1;
+  }
+});
